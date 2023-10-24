b22a69ed1dfe75c37f0fb0c988e6403cf56048f3
Guilherme Vale <guilherme.vale98@gmail.com>
2023-10-24 18:58:39 -0300

    Project initialization

---
+++ src/main/java/com/gui/flowershop/FlowershopAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gui.flowershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowershopAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowershopAppBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gui/flowershop/FlowershopAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gui.flowershop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowershopAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
