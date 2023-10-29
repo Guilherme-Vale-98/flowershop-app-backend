588646e548314e41866ad2ae769cc04596bae617
Guilherme Vale <guilherme.vale98@gmail.com>
2023-10-29 09:32:32 -0300

    Added findByNameContaning in ProductRepository

--- src/main/java/com/gui/flowershop/repositories/ProductRepository.java
+++ src/main/java/com/gui/flowershop/repositories/ProductRepository.java
@@ -1,7 +1,11 @@
 package com.gui.flowershop.repositories;
 
 import com.gui.flowershop.entities.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Page<Product> findByNameContaining(@RequestParam("name") String name, Pageable pageable);
 }
