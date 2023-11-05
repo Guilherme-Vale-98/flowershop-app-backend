cb4eb299347811ea65b263d61d65cb8297881180
Guilherme Vale <guilherme.vale98@gmail.com>
2023-11-04 21:44:12 -0300

    Added fiendByProductId on review repository

--- src/main/java/com/gui/flowershop/repositories/ReviewRepository.java
+++ src/main/java/com/gui/flowershop/repositories/ReviewRepository.java
@@ -1,10 +1,12 @@
 package com.gui.flowershop.repositories;
 
 import com.gui.flowershop.entities.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-
-
+    Page<Review> findByProductId(@RequestParam("product_Id") Long productId, Pageable pageable);
 
 }
